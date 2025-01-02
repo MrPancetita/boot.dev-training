728a4c6d26fab9e809dd2756af3b5f7176220eb4
MrPancetita <contreras.engineer@outlook.com>
2025-01-02 11:11:24 +0100

    ch7 l5 solved

---
+++ lessons/ch7/l5 - 759ff7b7-8e90-4b5c-88ce-f8531ff02582/main.py
@@ -0,0 +1,6 @@
+# https://www.boot.dev/lessons/759ff7b7-8e90-4b5c-88ce-f8531ff02582
+
+def check_swords_for_army(number_of_swords, number_of_soldiers):
+    if number_of_swords == number_of_soldiers:
+        return "correct amount"
+    return "incorrect amount"
