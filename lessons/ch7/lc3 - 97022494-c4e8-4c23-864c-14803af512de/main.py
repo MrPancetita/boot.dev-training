7a3063c15800834ab0286c02efe884a534afefcf
MrPancetita <contreras.engineer@outlook.com>
2025-01-02 13:53:20 +0100

    ch7 solved

--- lessons/ch7/lc3 - 97022494-c4e8-4c23-864c-14803af512de/main.py
+++ lessons/ch7/lc3 - 97022494-c4e8-4c23-864c-14803af512de/main.py
@@ -1,4 +1,5 @@
 # https://www.boot.dev/lessons/97022494-c4e8-4c23-864c-14803af512de
 
 def has_enough_energy(energy_available, distance_one_way, meters_per_energy):
-    pass
+    energy_needed = distance_one_way / meters_per_energy * 2
+    return energy_available >= energy_needed
